860d713ee57972f0d65ffac8b651967a50a2f533
DevEnchanter <minsoesantun@gmail.com>
2024-02-04 13:28:47 -0500

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="bg-gray-800 text-white p-4">
+            <p>© 2024 Custom PC Builder</p>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="bg-gray-800 text-white p-4">
+            <h1 className="text-lg">Custom PC Builder</h1>
+        </header>
+    );
+};
+
+export default Header;
